3f5c2cfe2e3455da2ab2bd2858190f0176eca6a0
Andrew D Urquhart <andrewurq@gmail.com>
2022-10-20 12:20:58 -0400

    making loop for find the dec prices

--- app.ts
+++ app.ts
@@ -1,36 +1,13 @@
+import { SPL } from "@prisma/client";
 import prisma from "./prisma/client";
 
 async function main() {
-  // I have data that begins before my historical data i declare a token price so I can define it as a fixed number later
+  // I have data that begins before my historical data For DEC token i declare a token price so I can define it as a fixed number later (8/10/2020)
   let firstDecPrice = new Date(2020, 7, 10);
 
-  const findAllOldSPL = await prisma.sPL.findMany({
-    where: {
-      Token: "DEC",
-      Created_Date: {
-        lte: firstDecPrice,
-      },
-    },
-  });
+  /* i update the prices for all tranactions that I have no data for below, this number or assumption could be wrong talk to Jesse */
 
-  let calculateDollars = findAllOldSPL.map((obj) => {
-    return {
-      inUSD: obj.Price * obj.Amount,
-    };
-  });
-
-  const updateAllJeCoding = await prisma.sPL.updateMany({
-    where: {
-      Token: "DEC",
-      Created_Date: {
-        lte: firstDecPrice,
-      },
-    },
-    data: {
-      inUSD: calculateDollars,
-    },
-  });
-  // const updateAllJeCoding = await prisma.sPL.updateMany({
+  // const updateOldSPLwithDefaultPrice = await prisma.sPL.updateMany({
   //   where: {
   //     Token: "DEC",
   //     Created_Date: {
@@ -42,38 +19,52 @@ async function main() {
   //   },
   // });
 
-  // console.log(findAllJeCoding);
-  //Note: JavaScript counts months from 0 to 11: January = 0 December = 11.
-  //   let firstDecPrice = new Date(2020, 7, 10);
+  console.log(firstDecPrice);
 
-  //   for (let element of findAllJeCoding) {
-  //     let strmonth = "";
-  //     if (element.Token === "DEC" && element.Created_Date <= firstDecPrice) {
-  //       /*this if controls logic for date before the first close price of the DEC token that I have in yahoo finance (8/10/2020) https://finance.yahoo.com/quote/DEC1-USD/history?period1=1594598400&period2=1666051200&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true*/
+  const updateSPLwithFindPrice = await prisma.sPL.findMany({
+    where: {
+      Token: "DEC",
+      Created_Date: {
+        gt: firstDecPrice,
+      },
+    },
+    select: {
+      id: true,
+      Created_Date: true,
+    },
+    take: 1,
+  });
 
-  //       tokenPrice = 0.000507;
-  //     } else {
-  //       let elementDate = element.Created_Date;
-  //       let date = elementDate.getUTCDate();
-  //       let month = elementDate.getUTCMonth();
-  //       let year = elementDate.getUTCFullYear();
+  console.log(updateSPLwithFindPrice);
 
-  //       if (month < 8) {
-  //         strmonth = "0" + month.toString();
-  //       } else if (month === 9) {
-  //         strmonth = "10";
-  //       } else strmonth = month.toString();
+  // for (let element of findAllJeCoding) {
+  //   let strmonth = "";
+  //   if (element.Token === "DEC" && element.Created_Date <= firstDecPrice) {
 
-  //       let dateStr = year + "-" + strmonth + "-" + date + "T00:00:00+00:00";
+  //     /*this if controls logic for date before the first close price of the DEC token that I have in yahoo finance (8/10/2020) https://finance.yahoo.com/quote/DEC1-USD/history?period1=1594598400&period2=1666051200&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true*/
 
-  //       const lookupPricebyDate = await prisma.history_price.findMany({
-  //         where: {
-  //           Date: dateStr,
-  //         },
-  //       });
+  //     tokenPrice = 0.000507;
+  //   } else {
+  //     let elementDate = element.Created_Date;
+  //     let date = elementDate.getUTCDate();
+  //     let month = elementDate.getUTCMonth();
+  //     let year = elementDate.getUTCFullYear();
 
-  //       console.log(element, lookupPricebyDate);
-  //     }
+  //     if (month < 8) {
+  //       strmonth = "0" + month.toString();
+  //     } else if (month === 9) {
+  //       strmonth = "10";
+  //     } else strmonth = month.toString();
+
+  //     let dateStr = year + "-" + strmonth + "-" + date + "T00:00:00+00:00";
+
+  //     const lookupPricebyDate = await prisma.history_price.findMany({
+  //       where: {
+  //         Date: dateStr,
+  //       },
+  //     });
+
+  //     console.log(element, lookupPricebyDate);
   //   }
   // }
 }
--- map.ts
+++ map.ts
@@ -1,4 +1,6 @@
-import prisma from "./client";
+import prisma from "./prisma/client";
+// seems like I was having trouble mapping over a number and creating many off of that in this file
+
 const v8 = require("v8");
 const totalHeapSize = v8.getHeapStatistics().total_available_size;
 
@@ -10,247 +12,223 @@ async function producerReward() {
     `total heap size (bytes) ${totalHeapSize}, (GB ~${totalHeapSizeInGB})`
   );
 
-  //   //find all the coding needed for every transaction type from the ledger table to apply it to the Accounting JE table where realized Gain/Loss does not need to be calculated
+  //find all the coding needed for every transaction type from the ledger table to apply it to the Accounting JE table where realized Gain/Loss does not need to be calculated
 
-  //   //tokens received is a seperate script to handle the tribe vs not tribe logic
+  //tokens received is a seperate script to handle the tribe vs not tribe logic
 
-  //   const findAllJeCoding = await prisma.ledger.findMany({
-  //     where: { Realized: false, Transaction_Type: "PRODUCER_REWARD" },
-  //     select: {
-  //       id: true,
-  //       Transaction_Type: true,
-  //       Dledger: true,
-  //       DLedger_SType: true,
-  //       Cledger: true,
-  //       CLedger_SType: true,
-  //       Realized: true,
-  //       Realized_Type: true,
-  //       Sale: true,
-  //     },
-  //   });
+  const findAllJeCoding = await prisma.ledger.findMany({
+    where: { Realized: false, Transaction_Type: "PRODUCER_REWARD" },
+    select: {
+      id: true,
+      Transaction_Type: true,
+      Dledger: true,
+      DLedger_SType: true,
+      Cledger: true,
+      CLedger_SType: true,
+      Realized: true,
+      Realized_Type: true,
+      Sale: true,
+    },
+  });
 
-  //   //the below for loop finds all the transactions that match the  transaction type (see the where clause)note all the tranaction types must exist in the legder table before this script will run correctly
-  //   for (const elementJeCoding of findAllJeCoding) {
+  //the below for loop finds all the transactions that match the  transaction type (see the where clause)note all the tranaction types must exist in the legder table before this script will run correctly
+  for (const elementJeCoding of findAllJeCoding) {
+    let debitLedgerType = elementJeCoding.Dledger;
+    let creditLedgerType = elementJeCoding.Cledger;
+    let creditLedger = elementJeCoding.CLedger_SType;
+    let debitLedger = elementJeCoding.DLedger_SType;
+    let transType = elementJeCoding.Transaction_Type;
+    //if the transaction type returns a true for the realized field that indicates realized needs to be calculated so the script will run the following script
 
-  //     let debitLedgerType = elementJeCoding.Dledger;
-  //     let creditLedgerType = elementJeCoding.Cledger;
-  //     let creditLedger = elementJeCoding.CLedger_SType;
-  //     let debitLedger = elementJeCoding.DLedger_SType;
-  //     let transType =  elementJeCoding.Transaction_Type
-  //     //if the transaction type returns a true for the realized field that indicates realized needs to be calculated so the script will run the following script
+    const findTransactionsTypeForThisLoop = await prisma.hive.findMany({
+      distinct: ["id"],
+      select: {
+        id: true,
+        Account_Ownership: true,
+        Asset_Type: true,
+        Asset: true,
+        Account: true,
+        Counterparty: true,
+        Proceed_Date: true,
+        Token_Price: true,
+        Price_Symbol: true,
+        Gross_Proceed: true,
+        Cost_of_Basis: true,
+        Net: true,
+        Transaction_Type: true,
+        Duration: true,
+      },
+      where: {
+        Transaction_Type: elementJeCoding?.Transaction_Type,
+      },
 
-  //     const findTransactionsTypeForThisLoop = await prisma.hive.findMany({
-  //       distinct: ["id"],
-  //       select: {
-  //         id: true,
-  //         Account_Ownership: true,
-  //         Asset_Type: true,
-  //         Asset: true,
-  //         Account: true,
-  //         Counterparty: true,
-  //         Proceed_Date: true,
-  //         Token_Price: true,
-  //         Price_Symbol: true,
-  //         Gross_Proceed: true,
-  //         Cost_of_Basis: true,
-  //         Net: true,
-  //         Transaction_Type: true,
-  //         Duration: true,
-  //       },
-  //       where: {
-  //         Transaction_Type: elementJeCoding?.Transaction_Type,
-  //       },
+      // if you want to do a test run uncomment the below line
+    });
 
-  //       // if you want to do a test run uncomment the below line
+    // console.log(findTransactionsTypeForThisLoop)
 
-  //     });
+    // let count = 0
 
-  //     // console.log(findTransactionsTypeForThisLoop)
+    //   for(let i = 0; i < updateFindToDebitJEdata.length; i++){
 
-  //     // let count = 0
+    //     count++
 
-  // //   for(let i = 0; i < updateFindToDebitJEdata.length; i++){
+    //   }
 
-  // //     count++
+    let updateFindEntity = findTransactionsTypeForThisLoop.map((obj) => {
+      return {
+        Entity: obj.Account_Ownership,
+      };
+    });
 
-  // //   }
+    let stringEntity = JSON.stringify(updateFindEntity);
 
-  //     let updateFindEntity = findTransactionsTypeForThisLoop.map(
-  //       obj => {
-  //           return {
-  //             "Entity" :obj.Account_Ownership,
-  //           }
-  //       }
-  //     );
+    let updateFindWallet = findTransactionsTypeForThisLoop.map((obj) => {
+      return {
+        Wallet: obj.Account,
+      };
+    });
 
-  //   let stringEntity = JSON.stringify(updateFindEntity)
+    let stringWallet = JSON.stringify(updateFindWallet);
 
-  //     let updateFindWallet = findTransactionsTypeForThisLoop.map(
-  //       obj => {
-  //           return {
-  //           "Wallet":obj.Account,
-  //           }
-  //       }
-  //     );
+    let updateFindCounter = findTransactionsTypeForThisLoop.map((obj) => {
+      return {
+        Counterparty: obj.Counterparty,
+      };
+    });
 
-  //     let stringWallet = JSON.stringify(updateFindWallet)
+    let stringCounter = JSON.stringify(updateFindCounter);
 
-  //     let updateFindCounter = findTransactionsTypeForThisLoop.map(
-  //       obj => {
-  //           return {
-  //               "Counterparty":obj.Counterparty,
-  //           }
-  //       }
-  //     );
+    let updateFindAsset = findTransactionsTypeForThisLoop.map((obj) => {
+      return {
+        Asset: obj.Price_Symbol,
+      };
+    });
 
-  //     let stringCounter = JSON.stringify(updateFindCounter)
+    let stringAsset = JSON.stringify(updateFindAsset);
 
-  //     let updateFindAsset = findTransactionsTypeForThisLoop.map(
-  //       obj => {
-  //           return {
-  //               "Asset": obj.Price_Symbol,
-  //           }
-  //       }
-  //     );
+    let updateFindDate = findTransactionsTypeForThisLoop.map((obj) => {
+      return {
+        Proceed_Date: obj.Proceed_Date,
+      };
+    });
 
-  //     let stringAsset = JSON.stringify(updateFindAsset)
+    let stringDate = JSON.stringify(updateFindDate);
 
-  //     let updateFindDate = findTransactionsTypeForThisLoop.map(
-  //       obj => {
-  //           return {
+    let updateFindDebit = findTransactionsTypeForThisLoop.map((obj) => {
+      return {
+        Debit: obj.Gross_Proceed,
+      };
+    });
 
-  //               "Proceed_Date": obj.Proceed_Date,
-  //           }
-  //       }
-  //     );
+    let stringDebit = JSON.stringify(updateFindDebit);
 
-  //     let stringDate = JSON.stringify(updateFindDate)
+    let updateFindCredit = findTransactionsTypeForThisLoop.map((obj) => {
+      return {
+        Credit: obj.Gross_Proceed,
+      };
+    });
 
-  //     let updateFindDebit = findTransactionsTypeForThisLoop.map(
-  //       obj => {
-  //           return {
+    let stringCredit = JSON.stringify(updateFindCredit);
 
-  //          "Debit": obj.Gross_Proceed,
+    let updateFindid = findTransactionsTypeForThisLoop.map((obj) => {
+      return {
+        CryptoDBid: obj.id,
+      };
+    });
 
-  //           }
-  //       }
-  //     );
+    let stringID = JSON.stringify(updateFindid);
 
-  //     let stringDebit = JSON.stringify(updateFindDebit)
+    let updateFindDuration = findTransactionsTypeForThisLoop.map((obj) => {
+      return {
+        Duration: obj.Duration,
+      };
+    });
 
-  //     let updateFindCredit = findTransactionsTypeForThisLoop.map(
-  //       obj => {
-  //           return {
-  //               "Credit" :obj.Gross_Proceed,
+    let stringDuration = JSON.stringify(updateFindDuration);
 
-  //       }}
-  //     );
+    let updateFindToCreditJEdata = findTransactionsTypeForThisLoop.map(
+      (obj) => {
+        return {
+          Entity: obj.Account_Ownership,
+          Wallet: obj.Account,
+          Counterparty: obj.Counterparty,
+          Asset: obj.Price_Symbol,
+          Proceed_Date: obj.Proceed_Date,
+          Ledger_Type1: elementJeCoding.Cledger,
+          Ledger_Type2: elementJeCoding.CLedger_SType,
+          Ledger_Name: obj.Transaction_Type,
+          Debit: obj.Gross_Proceed,
+          Credit: obj.Gross_Proceed,
+          CryptoDBid: obj.id,
+          Duration: obj.Duration,
+        };
+      }
+    );
 
-  //     let stringCredit = JSON.stringify(updateFindCredit)
+    const createAllDebit = await prisma.accountingJE.createMany({
+      data: {
+        Entity: stringEntity,
+        Wallet: stringWallet,
+        Counterparty: stringCounter,
+        Asset: stringAsset,
+        Proceed_Date: stringDate,
+        Ledger_Type1: debitLedgerType,
+        Ledger_Type2: debitLedger,
+        Ledger_Name: transType,
+        Debit: 1,
+        Duration: stringDuration,
+        CryptoDBid: 1,
+      },
+    });
 
-  //     let updateFindid = findTransactionsTypeForThisLoop.map(
-  //       obj => {
-  //           return {
-  //               "CryptoDBid": obj.id,
-  //           }
-  //       }
-  //     );
+    // const createAllCredit = await prisma.accountingJE.createMany({
+    //   data:updateFindToCreditJEdata
+    // })
 
-  //     let stringID = JSON.stringify(updateFindid)
+    //   const createAllDebit = await prisma.accountingJE.create({
+    //     data: {
+    //       Entity: createJELineElement?.Account_Ownership,
+    //       Wallet: createJELineElement?.Account,
+    //       Counterparty: createJELineElement?.Counterparty,
+    //       Asset: createJELineElement.Asset,
+    //       Proceed_Date: createJELineElement?.Proceed_Date,
+    //       Ledger_Type1: debitLedgerType,
+    //       Ledger_Type2: debitLedger,
+    //       Ledger_Name: createJELineElement.Transaction_Type,
+    //       Debit: createJELineElement?.Gross_Proceed,
+    //       Duration: createJELineElement?.Duration,
+    //       hive: {
+    //         connect: {
+    //           id: createJELineElement?.id,
+    //         },
+    //       },
+    //     },
+    //   });
 
-  //     let updateFindDuration = findTransactionsTypeForThisLoop.map(
-  //       obj => {
-  //           return {
-  //               "Duration": obj.Duration
-  //           }
-  //       }
-  //     );
+    //   const createAllCredit = await prisma.accountingJE.create({
+    //     data: {
+    //       Entity: createJELineElement?.Account_Ownership,
+    //       Wallet: createJELineElement?.Account,
+    //       Counterparty: createJELineElement?.Counterparty,
+    //       Asset: createJELineElement.Price_Symbol,
+    //       Proceed_Date: createJELineElement?.Proceed_Date,
+    //       Ledger_Type1: creditLedgerType,
+    //       Ledger_Type2: creditLedger,
+    //       Ledger_Name: createJELineElement.Transaction_Type,
+    //       Credit: createJELineElement?.Gross_Proceed,
+    //       Duration: createJELineElement?.Duration,
+    //       hive: {
+    //         connect: {
+    //           id: createJELineElement?.id,
+    //         },
+    //       },
+    //     },
+    //   });
 
-  //     let stringDuration = JSON.stringify(updateFindDuration)
-
-  //     let updateFindToCreditJEdata = findTransactionsTypeForThisLoop.map(
-  //       obj => {
-  //           return {
-
-  //               "Entity" :obj.Account_Ownership,
-  //               "Wallet":obj.Account,
-  //               "Counterparty":obj.Counterparty,
-  //               "Asset": obj.Price_Symbol,
-  //               "Proceed_Date": obj.Proceed_Date,
-  //               "Ledger_Type1": elementJeCoding.Cledger,
-  //               "Ledger_Type2":  elementJeCoding.CLedger_SType,
-  //               "Ledger_Name" : obj.Transaction_Type,
-  //               "Debit": obj.Gross_Proceed,
-  //               "Credit" :obj.Gross_Proceed,
-  //               "CryptoDBid": obj.id,
-  //               "Duration": obj.Duration
-  //           }
-  //       }
-  //     );
-
-  //     const createAllDebit = await prisma.accountingJE.createMany({
-  //       data: {
-  //         Entity: stringEntity,
-  //         Wallet: stringWallet,
-  //         Counterparty: stringCounter,
-  //         Asset: stringAsset,
-  //         Proceed_Date: stringDate,
-  //         Ledger_Type1: debitLedgerType,
-  //         Ledger_Type2: debitLedger,
-  //         Ledger_Name: transType,
-  //         Debit: 1,
-  //         Duration: stringDuration,
-  //         CryptoDBid: 1,
-  //       },
-  //     });
-
-  //     // const createAllCredit = await prisma.accountingJE.createMany({
-  //     //   data:updateFindToCreditJEdata
-  //     // })
-
-  //     //   const createAllDebit = await prisma.accountingJE.create({
-  //     //     data: {
-  //     //       Entity: createJELineElement?.Account_Ownership,
-  //     //       Wallet: createJELineElement?.Account,
-  //     //       Counterparty: createJELineElement?.Counterparty,
-  //     //       Asset: createJELineElement.Asset,
-  //     //       Proceed_Date: createJELineElement?.Proceed_Date,
-  //     //       Ledger_Type1: debitLedgerType,
-  //     //       Ledger_Type2: debitLedger,
-  //     //       Ledger_Name: createJELineElement.Transaction_Type,
-  //     //       Debit: createJELineElement?.Gross_Proceed,
-  //     //       Duration: createJELineElement?.Duration,
-  //     //       hive: {
-  //     //         connect: {
-  //     //           id: createJELineElement?.id,
-  //     //         },
-  //     //       },
-  //     //     },
-  //     //   });
-
-  //     //   const createAllCredit = await prisma.accountingJE.create({
-  //     //     data: {
-  //     //       Entity: createJELineElement?.Account_Ownership,
-  //     //       Wallet: createJELineElement?.Account,
-  //     //       Counterparty: createJELineElement?.Counterparty,
-  //     //       Asset: createJELineElement.Price_Symbol,
-  //     //       Proceed_Date: createJELineElement?.Proceed_Date,
-  //     //       Ledger_Type1: creditLedgerType,
-  //     //       Ledger_Type2: creditLedger,
-  //     //       Ledger_Name: createJELineElement.Transaction_Type,
-  //     //       Credit: createJELineElement?.Gross_Proceed,
-  //     //       Duration: createJELineElement?.Duration,
-  //     //       hive: {
-  //     //         connect: {
-  //     //           id: createJELineElement?.id,
-  //     //         },
-  //     //       },
-  //     //     },
-  //     //   });
-
-  //     // // if you want to see your script running on a larger data set
-  //     // console.log(elementJeCoding.Transaction_Type, " process completed");
-  //   }
+    // // if you want to see your script running on a larger data set
+    // console.log(elementJeCoding.Transaction_Type, " process completed");
+  }
 }
 
 ////----end of  producerReward function---------------------------------------
