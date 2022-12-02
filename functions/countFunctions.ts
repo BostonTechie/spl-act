66721469eb5c89536ee487a3b15d6414e3b9e745
Andrew D Urquhart <andrewurq@gmail.com>
2022-12-02 10:41:50 -0500

    :tada: have big and small groups to use

--- functions/countFunctions.ts
+++ functions/countFunctions.ts
@@ -24,20 +24,62 @@ export async function countTransactions(tokenName, accountName) {
 
   return countTrans;
 }
+async function fifoCalcRealized(countTransactions) {
+  /* 
+   Find the count of all Account,
+   Token combinations not have at least
+   one value, break those into large, versus
+   small types
+  */
 
-export async function noZeroTransactions() {
   let findAllAccounts = await promiseAccountsGlobal;
   let findAllTokens = await promiseTokensGlobal;
 
+  let smallI = 0;
+  let bigI = 0;
+
   for (let account of findAllAccounts) {
     for (let token of findAllTokens) {
+      if (bigI === 5) {
+        // reset i to send to specific function
+        bigI = 0;
+      }
+      if (smallI === 5) {
+        // reset i to send to specific function
+        smallI = 0;
+      }
+
       let countTrans = await prisma.sPL.count({
         where: {
           Token: token.Token,
           Account: account.Account,
         },
       });
-      console.log(account, token, countTrans);
+
+      if (countTrans != 0) {
+        if (countTrans != 0) {
+          if (countTrans >= 250000) {
+            let largeTObject = {
+              i: bigI,
+              count: countTrans,
+              account: account.Account,
+              token: token.Token,
+            };
+
+            console.log(largeTObject);
+            bigI++;
+          } else {
+            let smallTObject = {
+              i: smallI,
+              count: countTrans,
+              account: account.Account,
+              token: token.Token,
+            };
+            console.log(smallTObject);
+            smallI++;
+          }
+        }
+      }
     }
   }
 }
--- functions/fifoFunctions.ts
+++ functions/fifoFunctions.ts
@@ -146,12 +146,30 @@ async function test(func) {
 }
 
 async function fifoCalcRealized(countTransactions) {
+  /* 
+   Find the count of all Account,
+   Token combinations not have at least
+   one value, break those into large, versus
+   small types
+  */
+
   let findAllAccounts = await promiseAccountsGlobal;
   let findAllTokens = await promiseTokensGlobal;
-  let arrayOfCounts = [];
+
+  let smallI = 0;
+  let bigI = 0;
 
   for (let account of findAllAccounts) {
     for (let token of findAllTokens) {
+      if (bigI === 5) {
+        // reset i to send to specific function
+        bigI = 0;
+      }
+      if (smallI === 5) {
+        // reset i to send to specific function
+        smallI = 0;
+      }
+
       let countTrans = await prisma.sPL.count({
         where: {
           Token: token.Token,
@@ -160,19 +178,31 @@ async function fifoCalcRealized(countTransactions) {
       });
 
       if (countTrans != 0) {
-        if (countTrans <= 250000) {
+        if (countTrans != 0) {
+          if (countTrans >= 250000) {
+            let largeTObject = {
+              bigI: bigI,
+              count: countTrans,
+              account: account.Account,
+              token: token.Token,
+            };
+
+            console.log(largeTObject);
+            bigI++;
+          } else {
+            let smallTObject = {
+              smallI: smallI,
+              count: countTrans,
+              account: account.Account,
+              token: token.Token,
+            };
+            console.log(smallTObject);
+            smallI++;
+          }
         }
       }
     }
   }
-  console.log(arrayOfCounts);
-
-  // let countTrans = await prisma.sPL.count({
-  //   where: {
-  //     Token: token.Token,
-  //     Account: account.Account,
-  //   },
-  // });
 }
 
 async function fifoUpdateColumn() {
